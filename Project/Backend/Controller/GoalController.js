0d1417a1b35959a6bc2a551af949b78bb3dbe89a
LandgeRushikesh <rushilandge12@gmail.com>
2026-02-11 18:00:19 +0530

    feat: implement goal management with CRUD operations and error handling middleware; add MongoDB connection

--- Project/Backend/Controller/GoalController.js
+++ Project/Backend/Controller/GoalController.js
@@ -1,23 +1,30 @@
+import asyncHandler from 'express-async-handler'
+
 // @desc Get goals
 // @routes GET /api/goals 
-export const getGoals = (req, res) => {
+export const getGoals = asyncHandler((req, res) => {
     res.status(200).json({ message: "Get Goals" })
-}
+})
 
 // @desc Set goals
 // @routes POST /api/goals 
-export const SetGoals = (req, res) => {
+export const SetGoals = asyncHandler((req, res) => {
+    if (!req.body?.text) {
+        res.status(400)
+        throw new Error("Please Add Text Field")
+    }
+
     res.status(200).json({ message: "Set Goals" })
-}
+})
 
 // @desc Update goals
 // @routes PUT /api/goals/:id
-export const updateGoal = (req, res) => {
+export const updateGoal = asyncHandler((req, res) => {
     res.status(200).json({ message: `Update goal with id ${req.params.id}` })
-}
+})
 
 // @desc Delete goals
 // @routes DELETE /api/goals/:id 
-export const deleteGoal = (req, res) => {
+export const deleteGoal = asyncHandler((req, res) => {
     res.status(200).json({ message: `Delete goal with id ${req.params.id}` })
-}
+})
--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import goalRoutes from './Routes/GoalRoutes.js'
+import { errorHandler } from './Middleware/ErrorHandler.js'
+import connectDB from './DB/Config.js'
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
+// Database Connection
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // routes use
 app.use('/api/goals/', goalRoutes)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
