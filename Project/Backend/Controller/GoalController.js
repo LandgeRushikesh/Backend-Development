146824f0df5b3dc5c14e6eb31e9e01d3a9d0ea7f
LandgeRushikesh <rushilandge12@gmail.com>
2026-02-14 22:08:38 +0530

    feat: enhance goal routes with user authentication and authorization checks

--- Project/Backend/Controller/GoalController.js
+++ Project/Backend/Controller/GoalController.js
@@ -5,7 +5,7 @@ import { Goal } from '../model/goalModel.js'
 // @desc Get goals
 // @routes GET /api/goals 
 export const getGoals = asyncHandler(async (req, res) => {
-    const goal = await Goal.find()
+    const goal = await Goal.find({ user: req.user.id })
     res.status(200).json(goal)
 })
 
@@ -18,7 +18,8 @@ export const SetGoals = asyncHandler(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
 
     res.status(200).json(goal)
@@ -27,19 +28,44 @@ export const SetGoals = asyncHandler(async (req, res) => {
 // @desc Update goals
 // @routes PUT /api/goals/:id
 export const updateGoal = asyncHandler(async (req, res) => {
-    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    const id = req.params.id
+    const goal = await Goal.findById(id)
 
-    if (!updatedGoal) {
+    if (!goal) {
         res.status(404)
         throw new Error("Goal not Found")
     }
 
+    if (goal.user.toString() !== req.user.id) {
+        res.status(403)
+        throw new Error("You Cannot Update this Goal ")
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(id, req.body, { new: true })
+    if (!updatedGoal) {
+        res.status(400)
+        throw new Error("Goal not Found")
+    }
+
     res.status(200).json(updatedGoal)
+
 })
 
 // @desc Delete goals
 // @routes DELETE /api/goals/:id 
 export const deleteGoal = asyncHandler(async (req, res) => {
+    const id = req.params.id
+    const goal = await Goal.findById(id)
+    if (!goal) {
+        res.status(404)
+        throw new Error("Cannot Find Goal...")
+    }
+
+    if (goal.user.toString() !== req.user.id) {
+        res.status(403)
+        throw new Error("You Cannot delete this Goal")
+    }
+
     const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
 
     if (!deletedGoal) {
--- Project/Backend/Routes/GoalRoutes.js
+++ Project/Backend/Routes/GoalRoutes.js
@@ -1,18 +1,19 @@
 import express from 'express'
 import { deleteGoal, getGoals, SetGoals, updateGoal } from '../Controller/GoalController.js'
+import protect from "../Middleware/AuthMiddleware.js"
 
 const router = express.Router()
 
 // get goals
-router.get('/', getGoals)
+router.get('/', protect, getGoals)
 
 // Set Goals
-router.post('/', SetGoals)
+router.post('/', protect, SetGoals)
 
 // Update Goal
-router.put('/:id', updateGoal)
+router.put('/:id', protect, updateGoal)
 
 // Delete Goal
-router.delete('/:id', deleteGoal)
+router.delete('/:id', protect, deleteGoal)
 
 export default router
